89ff66f36e4cc1727f6ed28c33b8e328f1cdc294
opitz21254 <113393776+opitz21254@users.noreply.github.com>
2024-09-05 20:23:05 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.WriteLine("Hello, World!");
+
+// Measue 2
+Console.Beep(784, 1000);
+Console.Beep(733, 1000);
+Console.Beep(495, 1000);
+
+Console.Beep(660, 1000);
+Console.Beep(587, 1000);
+Console.Beep(392, 1000);
+
+// Measure 3
+Console.Beep(440, 2500);
+Console.Beep(495, 500);
+Console.Beep(523, 2000);
+Console.Beep(37, 1000);
+
+// Measure 4
+Console.Beep(324, 2000);
+Console.Beep(365, 500);
+Console.Beep(384, 500);
+Console.Beep(432, 500);
+Console.Beep(486, 500);
+Console.Beep(512, 500);
+Console.Beep(576, 500);
+Console.Beep(648, 500);
+Console.Beep(729, 500);
+
+//Measure 5
+Console.Beep(972, 3500);
+Console.Beep(37, 2500);
+
+//Measure 6
+Console.Beep(784, 1000);
+Console.Beep(733, 1000);
+Console.Beep(495, 1000);
+
+Console.Beep(660, 1000);
+Console.Beep(587, 1000);
+Console.Beep(392, 1000);
+
+
+//Measure 7
+Console.Beep(457, 2500);
+Console.Beep(486, 500);
+Console.Beep(539, 2000);
+Console.Beep(37, 1000);
+
+//Measuer 8
+Console.Beep(365, 1500);
+Console.Beep(405, 500);
+Console.Beep(455, 500);
+Console.Beep(486, 500);
+Console.Beep(539, 500);
+Console.Beep(576, 500);
+Console.Beep(648, 500);
+Console.Beep(729, 500);
+Console.Beep(809, 500);
+
+//Measure 9
+Console.Beep(1152, 3500);
+Console.Beep(37, 2500);
+
+//Measure 10
+Console.Beep(1157, 1000);
+Console.Beep(972, 1000);
+Console.Beep(768, 1000);
+
+Console.Beep(648, 1000);
+Console.Beep(729, 1000);
+Console.Beep(768, 1000);
+
+//Measure 11
+Console.Beep(576, 2500);
+Console.Beep(648, 500);
+Console.Beep(729, 2000);
+Console.Beep(37, 1000);
+
+//Measure 12
+Console.Beep(1079, 1000);
+Console.Beep(1728, 1000);
+Console.Beep(1458, 1000);
+
+
+// Measure 13
+Console.Beep(440, 2500);
+Console.Beep(495, 500);
+Console.Beep(523, 2000);
+Console.Beep(37, 1000);
